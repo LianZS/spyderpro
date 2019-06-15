4a2d322c3c3a0945e4a8aa2c3d65eef0747b4ef3
LianZS <2214057556@qq.com>
2019-06-15 10:43:18 +0800

    test

--- spyderpro/test/test_main.py
+++ spyderpro/test/test_main.py
@@ -1,6 +1,15 @@
 import csv
 import time
 from spyderpro.LocationBigData.PeopleFlow import ScencePeopleFlow
+from spyderpro.Weather.WeatherLishi import WeatherData
+
+
+def test_weather():
+    w = WeatherData()
+    datalist = w.get_province_link()
+    datalist = w.get_city_pastlink(url=datalist[7]['url'])
+    w.get_city_allpartition(datalist[0]['url'])
+
 
 if __name__ == "__main__":
     f = open('/Users/darkmoon/Project/SpyderPro/spyderpro/testdata/ScenceData.csv', "r")
@@ -8,8 +17,8 @@ if __name__ == "__main__":
     read = csv.reader(f)
     pep = ScencePeopleFlow()
     title = read.__next__()
-    filename = time.strftime("%Y-%d-%m", time.localtime(time.time()-3600*24))
-    f2 = open("/Volumes/Tigo/数据集/%s.csv"% filename, 'a+', newline="")
+    filename = time.strftime("%Y-%d-%m", time.localtime(time.time() - 3600 * 24))
+    f2 = open("/Volumes/Tigo/数据集/%s.csv" % filename, 'a+', newline="")
     w = csv.writer(f2)
     for item in read:
         data = list()
