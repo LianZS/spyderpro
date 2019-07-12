b57bbf8741197aad2f99e3435f5930235e664c8c
LianZS <2214057556@qq.com>
2019-07-12 23:42:02 +0800

    任务队列配置完毕

--- celerytask/celeryconfig.py
+++ celerytask/celeryconfig.py
@@ -1,15 +1,24 @@
-from celery import Celery
 import ssl
+from kombu import Queue, Exchange
+from celerytask import task1, task2, task3, task4, task5
 
 BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/1'
-CELERY_IMPORTS = ()  # 导入指定任务墨模块
+CELERY_IMPORTS = (task1, task2, task3, task4, task5)  # 导入指定任务墨模块
 
 CELERYBEAT_SCHEDULE = {
 
 }  # 默认的定时调度程序
-CELERY_QUEUES = ()  # 自定义队列
-CELERY_TASK_DEFAULT_QUEUE = ''  # 默认队列
+CELERY_QUEUES = (
+    Queue('default', exchange=Exchange('default', type='direct')),
+    Queue('financial', routing_key='celerytask.task1.#', exchange=Exchange('task1', type='direct')),
+    Queue('Internet', routing_key='celerytask.task2.#', exchange=Exchange('task2', type='direct')),
+    Queue('location', routing_key='celerytask.task3.#', exchange=Exchange('task3', type='direct')),
+    Queue('traffic', routing_key='celerytask.task4.#', exchange=Exchange('task4', type='direct')),
+    Queue('weather', routing_key='celerytask.task4.#', exchange=Exchange('task4', type='direct')),
+
+)  # 自定义队列
+CELERY_TASK_DEFAULT_QUEUE = 'default'  # 默认队列
 CELERY_ROUTES = {
 
 }  # 路由器列表
