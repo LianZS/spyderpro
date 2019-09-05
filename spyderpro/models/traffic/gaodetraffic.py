1954011b29c7f4c4374f569a43be75c039eaa792
LianZS <2214057556@qq.com>
2019-09-05 17:07:55 +0800

    标准化

--- spyderpro/models/traffic/gaodetraffic.py
+++ spyderpro/models/traffic/gaodetraffic.py
@@ -2,10 +2,10 @@ import requests
 import json
 import time
 from urllib.parse import urlencode
-from typing import Iterator
+from typing import Iterator, Dict
 from threading import Semaphore, Thread
 from queue import Queue
-from spyderpro.models.traffic.multhread import MulitThread
+from concurrent.futures import ThreadPoolExecutor
 from spyderpro.models.traffic.trafficinterface import Traffic
 from spyderpro.instances.trafficclass import TrafficClass, Road, Year
 
@@ -43,29 +43,29 @@ class GaodeTraffic(Traffic):
             返回TrafficClass类实例
 
         """
-        url = "http://report.amap.com/ajax/cityHourly.do?cityCode=" + str(citycode)
+        str_href = "http://report.amap.com/ajax/cityHourly.do?cityCode=" + str(citycode)
         try:
-            data = self.s.get(url=url, headers=self.headers)
-            g = json.loads(data.text)
-        except Exception as e:
+            response = self.s.get(url=str_href, headers=self.headers)
+            json_data = json.loads(response.text)
+        except ConnectionError as e:
 
             print("编号%d--网络链接error:%s" % (citycode, e))
 
             return None
-        today = time.strftime("%Y-%m-%d", time.localtime())  # 今天的日期
+        date_today = time.strftime("%Y-%m-%d", time.localtime())  # 今天的日期
 
-        yesterday = time.strftime("%Y-%m-%d", time.localtime(time.time() - 3600 * 24))  # 昨天的日期
-        date = yesterday
+        date_yesterday = time.strftime("%Y-%m-%d", time.localtime(time.time() - 3600 * 24))  # 昨天的日期
+        date = date_yesterday
         # 含有24小时的数据
-        for item in g:
-            detailtime = time.strftime("%H:%M", time.localtime(int(item[0]) / 1000))
-            if detailtime == '00:00':
-                date = today
+        for item in json_data:
+            detail_time = time.strftime("%H:%M", time.localtime(int(item[0]) / 1000))
+            if detail_time == '00:00':
+                date = date_today
 
-            ddate = int(date.replace("-", ""))  # 日期
-            iindex = float(item[1])  # 拥堵指数
-            detailtime = detailtime + ":00"  # 具体时刻
-            yield TrafficClass(citycode, ddate, iindex, detailtime)
+            int_date = int(date.replace("-", ""))  # 日期
+            float_index = float(item[1])  # 拥堵指数
+            detail_time = detail_time + ":00"  # 具体时刻
+            yield TrafficClass(citycode, int_date, float_index, detail_time)
 
     # 道路数据获取
     def roaddata(self, citycode: int) -> Iterator[Road]:
@@ -90,76 +90,80 @@ class GaodeTraffic(Traffic):
             direction = item['dir']  # 道路方向
             bounds = json.dumps({"coords": item['coords']})  # 道路经纬度数据
             num = eval(data)['num']
-            rate=float(item['index'])
+            rate = float(item['index'])
             road = Road(pid=citycode, roadname=roadname, speed=speed, dircetion=direction, bounds=bounds, data=data,
-                        num=num,rate=rate)
+                        num=num, rate=rate)
 
             yield road
 
-    def __roads(self, citycode: int) -> dict:
+    def __roads(self, citycode: int) -> Dict:
         """
         获取道路信息包，包括道路pid，道路名，道路方向，速度
         :param citycode:城市id
         :return:dict
         """
 
-        req = {
+        dict_parameter = {
             "roadType": 0,
             "timeType": 0,
             "cityCode": citycode
         }
-        url = "https://report.amap.com/ajax/roadRank.do?" + urlencode(req)
-        data = self.s.get(url=url, headers=self.headers)
+        str_href = "https://report.amap.com/ajax/roadRank.do?" + urlencode(dict_parameter)
+        try:
+            response = self.s.get(url=str_href, headers=self.headers)
+        except requests.exceptions.ConnectionError:
+            print("网络错误")
+            return {}
 
         try:
-            route = json.loads(data.text)  # 道路信息包
-        except Exception as e:
-            print(e)
+            json_data = json.loads(response.text)  # 道路信息包
+        except AttributeError:
             return {}
         list_id = []  # 记录道路pid
         list_roadname = []  # 记录道路名
         list_dir = []  # 记录道路方向
         list_speed = []  # 记录速度
-        for item in route["tableData"]:
+        for item in json_data["tableData"]:
             list_roadname.append(item["name"])  # 道路名
             list_dir.append(item["dir"])  # 方向
             list_speed.append(item["speed"])  # 速度
             list_id.append(item["id"])  # 道路pid
-        dic_collections = dict()  # 存放所有数据
-        dic_collections["route"] = route['tableData']
-        dic_collections["listId"] = list_id
-        dic_collections["listRoadName"] = list_roadname
-        dic_collections["listDir"] = list_dir
-        dic_collections["listSpeed"] = list_speed
+        dict_collections = dict()  # 存放所有数据
+        dict_collections["route"] = json_data['tableData']
+        dict_collections["listId"] = list_id
+        dict_collections["listRoadName"] = list_roadname
+        dict_collections["listDir"] = list_dir
+        dict_collections["listSpeed"] = list_speed
 
-        return dic_collections
+        return dict_collections
 
     def __realtimeroad(self, dic: dict, citycode: int) -> Iterator[dict]:
         """
         请求10条道路路实时路况数据
-        :param dic:
-        :param citycode:
-        :return: dict
+        :param dic:道路信息字典
+        :param citycode:城市id
         """
-        req = {
+        dict_parameter = {
             "roadType": 0,
             "timeType": 0,
             "cityCode": citycode,
             'lineCode': ''
 
         }
-        url = "https://report.amap.com/ajax/roadDetail.do?" + urlencode(req)
-
+        str_href = "https://report.amap.com/ajax/roadDetail.do?" + urlencode(dict_parameter)
+        pool = ThreadPoolExecutor(max_workers=10)
+        # 存放任务对象
+        reuslt_list = list()
         for pid, i in zip(dic["listId"],
                           range(0, (dic["listId"]).__len__())):
-            roadurl = url + str(pid)
-            self.wait.acquire()
-            Thread(target=self.__realtime_roaddata, args=(roadurl, i,)).start()
-        while self.quitcount:
-            data = self.dataqueue.get()
-            yield data
+            str_road_url = str_href + str(pid)
+            t = pool.submit(self.__realtime_roaddata, str_road_url, i)
+            reuslt_list.append(t)
+        for t in reuslt_list:
+            # 返回结果
+            yield t.result()
 
-    def __realtime_roaddata(self, roadurl, i):
+    def __realtime_roaddata(self, road_url: str, i: int):
         """
         具体请求某条道路的数据
         :param roadurl: 道路数据链接
@@ -169,28 +173,28 @@ class GaodeTraffic(Traffic):
         """
 
         try:
-            data = self.s.get(url=roadurl, headers=self.headers)
-            g = json.loads(data.text)  # 拥堵指数
-        except Exception as e:
-            print(e)
-            self.dataqueue.put({"data": None, "num": i})
-            self.lock.acquire()
-            self.quitcount -= 1
-            self.lock.release()
-            return
-        data = []  # 拥堵指数
-        time_list = []  # 时间
-        for item in g:
-            time_list.append(time.strftime("%H:%M", time.strptime(time.ctime(int(item[0] / 1000 + 3600 * 8)))))
-            data.append(item[1])
+            response = self.s.get(url=road_url, headers=self.headers)
+            json_data = json.loads(response.text)  # 拥堵指数
+        except requests.exceptions.ConnectionError:
+            print("网络链接error")
+            return {"data": None, "num": i}
+        except requests.exceptions.ChunkedEncodingError:
+            print("网络链接error")
+            return {"data": None, "num": i}
+        except AttributeError:
+            print("数据格式错误")
+            return {"data": None, "num": i}
+
+        list_data = []  # 拥堵指数
+        list_time = []  # 时间
+        for item in json_data:
+            list_time.append(time.strftime("%H:%M", time.strptime(time.ctime(int(item[0] / 1000 + 3600 * 8)))))
+            list_data.append(item[1])
         # {排名，时间，交通数据}
-        realdata = {"num": i, "time": time_list, "data": data}
-        self.dataqueue.put({"data": realdata, "num": i})
-        self.wait.release()
-        self.lock.acquire()
-        self.quitcount -= 1
-        self.lock.release()
-        return
+        real_data = {"num": i, "time": list_time, "data": list_data}
+        result = {"data": real_data, "num": i}
+
+        return result
 
     def yeartraffic(self, citycode: int, year: int = int(time.strftime("%Y", time.localtime())),
                     quarter: int = int(time.strftime("%m", time.localtime())) / 3) -> Iterator[Year]:
@@ -208,23 +212,34 @@ class GaodeTraffic(Traffic):
             quarter = int(quarter) + 1
         else:
             quarter = int(quarter)
-        url = "http://report.amap.com/ajax/cityDailyQuarterly.do?"
+        str_href = "http://report.amap.com/ajax/cityDailyQuarterly.do?"
 
         # year键表示哪一年 的数据
-        req = {
+        dict_parameter = {
             "cityCode": citycode,
             "year": year,  # 年份
             "quarter": quarter  # 第几季
         }
 
-        url = url + urlencode(req)
+        url = str_href + urlencode(dict_parameter)
 
         try:
-            data = self.s.get(url=url, headers=self.headers)
-            g = eval(data.text)
+            response = self.s.get(url=url, headers=self.headers)
+            json_data = eval(response.text)
         except SyntaxError:
             print("高德地图年度数据请求失败！")
-            return None
-        for date, index in zip(g["categories"], g['serieData']):
+            return []
+        except requests.exceptions.ConnectionError:
+            print("网络链接error")
+            return []
+        except requests.exceptions.ChunkedEncodingError:
+            print("网络链接error")
+            return []
+        except AttributeError:
+            print("数据格式错误")
+            return []
+        for date, index in zip(json_data["categories"], json_data['serieData']):
             yield Year(pid=citycode, date=int(date.replace("-", "")), index=index)
+
+
 
