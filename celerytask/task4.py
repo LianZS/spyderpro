44394a903d6a3cb16d9125e84edd2b4f5ec91546
LianZS <2214057556@qq.com>
2019-09-06 19:05:12 +0800

    标准化

--- celerytask/task4.py
+++ celerytask/task4.py
@@ -5,7 +5,7 @@ from .celeryconfig import app
 traffic = ManagerTraffic()
 
 
-@app.task(queue='traffic')
+@app.task(queue='daily_traffic')
 def monitoring_dailycitytraffic():
     """
         监控城市实时交通拥堵情况
@@ -13,7 +13,7 @@ def monitoring_dailycitytraffic():
     traffic.manager_city_traffic()
 
 
-@app.task(queue='traffic')
+@app.task(queue='road_traffic')
 def monitoring_roadtraffic():
     """
         监控城市道路实时交通拥堵情况
@@ -21,7 +21,7 @@ def monitoring_roadtraffic():
     traffic.manager_city_road_traffic()
 
 
-@app.task(queue='traffic')
+@app.task(queue='year_traffic')
 def monitoring_yeartraffic():
     """
         监控城市日交通拥堵情况
@@ -29,11 +29,10 @@ def monitoring_yeartraffic():
     traffic.manager_city_year_traffic()
 
 
-@app.task(queue='traffic')
+@app.task(queue='clear_traffic')
 def clear_roadtraffic():
     """
     清除道路数据
 
     """
     traffic.clear_road_data()
-
