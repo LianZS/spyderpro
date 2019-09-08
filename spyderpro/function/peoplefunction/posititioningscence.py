1878d461c9f0fb8460d46280d8451e748b76db0f
LianZS <2214057556@qq.com>
2019-09-08 14:31:36 +0800

    缓存

--- spyderpro/function/peoplefunction/posititioningscence.py
+++ spyderpro/function/peoplefunction/posititioningscence.py
@@ -1,11 +1,8 @@
-import time
 import datetime
-import redis
 from typing import List
-from setting import *
+from spyderpro.managerfunction.redis_connect import RedisConnectPool
 from spyderpro.managerfunction.mysql_connect import ConnectPool
 from spyderpro.models.locationdata.scencepeople import ScencePeopleFlow
-from spyderpro.models.weather.weather import WeatherForect
 from concurrent.futures import ThreadPoolExecutor
 from spyderpro.portconnect.sqlconnect import MysqlOperation
 
@@ -49,16 +46,33 @@ class ScenceFlow(Parent):
         flow = ScencePeopleFlow()
         # 获取数据
         instances = flow.peopleflow_info(peoplepid, ddate)
+        # 缓冲数据
+        self.__redis_cache(instances)
         # 过滤已存在的数据
         info = self.__filter_peopleflow(db, instances, ddate, peoplepid)
 
         return info
 
+    def __redis_cache(self, data_objs):
+        """
+        缓存数据
+        :param data_objs: 数据实例
+        :return:
+        """
+        dict_data = dict()
+        for obj in data_objs:
+            pid = obj.region_id
+            detail_time = obj.detailTime
+            num = obj.num
+            dict_data[detail_time] = num
+        redis_pool = RedisConnectPool(max_workers=1)
+        redis_pool.hashset(name="scence:11", mapping=dict_data)
+
     # 检查数据库是否存在部分数据，存在则不再插入
-    def __filter_peopleflow(self, db, objs, ddate: int, peoplepid: int) -> List[Positioning]:
+    def __filter_peopleflow(self, db_connect, objs, ddate: int, peoplepid: int) -> List[Positioning]:
         """
         检查数据库是否存在部分数据，存在则不再插入
-        :param db:  数据库实例
+        :param db_connect:  数据库实例
         :param objs: Positioning迭代器
         :param ddate: 日期
         :param peoplepid: 数据库查询条件
@@ -67,7 +81,7 @@ class ScenceFlow(Parent):
 
         sql = "select ttime from digitalsmart.scenceflow where pid={0} and  ddate={1} ".format(peoplepid,
                                                                                                ddate)
-        cursor = self.get_cursor(db, sql)  # 从数据库获取当天已存在的数据
+        cursor = self.get_cursor(db_connect, sql)  # 从数据库获取当天已存在的数据
         if cursor == "error":
             return []
         data = cursor.fetchall()  # 从数据库获取已经存在的数据
@@ -85,101 +99,100 @@ class ScenceFlow(Parent):
                 dic.pop(detailtime)
             except KeyError:
                 continue
-        print(dic)
         return list(dic.values())
 
 
-class Weather(Parent):
-    @classmethod
-    def weather(cls, weatherpidlist):
-        """
-        获取天气数据
-        :param weatherpidlist: 天气id列表
-        :return:
-        """
-        while True:
-            if cls.instance is None:
-                cls.instance = super().__new__(cls)
-            cls.instance.programmerpool(cls.instance.getweather, weatherpidlist)
-
-    # def getweather(self, weatherpid):
-    #     """
-    #     获取天气数据
-    #     :param weatherpid:
-    #     :return:
-    #     """
-    #     pool = ConnectPool(max_workers=1)
-    #     db = pymysql.connect(host=host, user=user, password=password, database=scencedatabase,
-    #                          port=port)
-    #
-    #     sql = "select WeatherTablePid from webdata.ScenceInfoData where  WeatherPid=" \
-    #           + "'" + weatherpid + "';"
-    #
-    #     cursor = self.get_cursor(db, sql)
-    #     if cursor is None:
-    #         return False
-    #     weathertablepid = cursor.fetchone()[0]
-    #     cursor.close()
-    #     wea = WeatherForect()
-    #     info = wea.weatherforcest(weatherpid)
-    #
-    #     # 每次爬取都是获取未来7天的数据，所以再次爬取时只需要以此刻为起点，看看数据库存不存在7天后的数据
-    #     date = time.strftime('%Y-%m-%d', time.localtime(
-    #         time.time() + 7 * 3600 * 24))
-    #     info = self.__dealwith_weather(info, db, weathertablepid, date)
-    #     for item in info:
-    #         date = item['date']
-    #         detailtime = item['detailTime']
-    #         state = item['state']
-    #         temperature = item['temperature']
-    #         wind = item['wind']
-    #         sql = "insert into  webdata.weather(pid_id,date,detailTime,state,temperature,wind) " \
-    #               "values('%d','%s','%s','%s','%s','%s');" % (
-    #                   weathertablepid, date, detailtime, state, temperature, wind)
-    #         if not self.loaddatabase(db, sql):
-    #             print("插入失败！")
-    #             continue
-    #
-    #     db.close()
-    #     print("success")
-    #     return True
-
-    # 有bug
-    def __dealwith_weather(self, info, db, pid, date) -> list:
-        """
-        过滤已存在天气数据
-        :param info:
-        :param db:
-        :param pid:
-        :param date:
-        :return:list
-        """
-
-        sql = "select  date,detailTime from webdata.weather where pid_id=" + str(
-            pid) + " and date =str_to_date('" + date + "','%Y-%m-%d');"
-        cursor = self.get_cursor(db, sql)
-        if cursor is None:
-            cursor.close()
-            return info
-        data = cursor.fetchall()
-
-        if len(data) == 0:
-            cursor.close()
-            return info
-        lis = []
-
-        for item in info:
-            if item['date'] != date:
-                continue
-            lis.append(dict(zip(item.values(), item.keys())))
-        info = lis
-        for olddata in data:
-            self.filter(info, olddata[0], olddata[1])
-        lis = []
-        for item in info:
-            lis.append(dict(zip(item.values(), item.keys())))
-        info = lis
-        return info
+# class Weather(Parent):
+#     @classmethod
+#     def weather(cls, weatherpidlist):
+#         """
+#         获取天气数据
+#         :param weatherpidlist: 天气id列表
+#         :return:
+#         """
+#         while True:
+#             if cls.instance is None:
+#                 cls.instance = super().__new__(cls)
+#             cls.instance.programmerpool(cls.instance.getweather, weatherpidlist)
+#
+#     # def getweather(self, weatherpid):
+#     #     """
+#     #     获取天气数据
+#     #     :param weatherpid:
+#     #     :return:
+#     #     """
+#     #     pool = ConnectPool(max_workers=1)
+#     #     db = pymysql.connect(host=host, user=user, password=password, database=scencedatabase,
+#     #                          port=port)
+#     #
+#     #     sql = "select WeatherTablePid from webdata.ScenceInfoData where  WeatherPid=" \
+#     #           + "'" + weatherpid + "';"
+#     #
+#     #     cursor = self.get_cursor(db, sql)
+#     #     if cursor is None:
+#     #         return False
+#     #     weathertablepid = cursor.fetchone()[0]
+#     #     cursor.close()
+#     #     wea = WeatherForect()
+#     #     info = wea.weatherforcest(weatherpid)
+#     #
+#     #     # 每次爬取都是获取未来7天的数据，所以再次爬取时只需要以此刻为起点，看看数据库存不存在7天后的数据
+#     #     date = time.strftime('%Y-%m-%d', time.localtime(
+#     #         time.time() + 7 * 3600 * 24))
+#     #     info = self.__dealwith_weather(info, db, weathertablepid, date)
+#     #     for item in info:
+#     #         date = item['date']
+#     #         detailtime = item['detailTime']
+#     #         state = item['state']
+#     #         temperature = item['temperature']
+#     #         wind = item['wind']
+#     #         sql = "insert into  webdata.weather(pid_id,date,detailTime,state,temperature,wind) " \
+#     #               "values('%d','%s','%s','%s','%s','%s');" % (
+#     #                   weathertablepid, date, detailtime, state, temperature, wind)
+#     #         if not self.loaddatabase(db, sql):
+#     #             print("插入失败！")
+#     #             continue
+#     #
+#     #     db.close()
+#     #     print("success")
+#     #     return True
+#
+#     # 有bug
+#     def __dealwith_weather(self, info, db, pid, date) -> list:
+#         """
+#         过滤已存在天气数据
+#         :param info:
+#         :param db:
+#         :param pid:
+#         :param date:
+#         :return:list
+#         """
+#
+#         sql = "select  date,detailTime from webdata.weather where pid_id=" + str(
+#             pid) + " and date =str_to_date('" + date + "','%Y-%m-%d');"
+#         cursor = self.get_cursor(db, sql)
+#         if cursor is None:
+#             cursor.close()
+#             return info
+#         data = cursor.fetchall()
+#
+#         if len(data) == 0:
+#             cursor.close()
+#             return info
+#         lis = []
+#
+#         for item in info:
+#             if item['date'] != date:
+#                 continue
+#             lis.append(dict(zip(item.values(), item.keys())))
+#         info = lis
+#         for olddata in data:
+#             self.filter(info, olddata[0], olddata[1])
+#         lis = []
+#         for item in info:
+#             lis.append(dict(zip(item.values(), item.keys())))
+#         info = lis
+#         return info
 
 
 if __name__ == "__main__":
