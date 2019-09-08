3781ace8ce988db7c71fae9bb6e061c4d37e71c1
LianZS <2214057556@qq.com>
2019-09-08 14:19:04 +0800

    注释

--- spyderpro/managerfunction/redis_connect.py
+++ spyderpro/managerfunction/redis_connect.py
@@ -117,12 +117,23 @@ class RedisConnectPool(object):
 
     @check_state
     def strlen(self, name) -> int:
+        """
+        redis String 值长度
+        :param name: 键名
+        :return:
+        """
 
         length: int = self._redis_pool.strlen(name)
         return length
 
     @check_state
-    def hashset(self, name, mapping) -> bool:
+    def hashset(self, name, mapping: dict) -> bool:
+        """
+        哈希添加
+        :param name:key
+        :param mapping: 键值对
+        :return:
+        """
         pipe = self._redis_pool.pipeline()
         result = self._redis_pool.hmset(name, mapping)
         pipe.execute()
@@ -130,21 +141,43 @@ class RedisConnectPool(object):
 
     @check_state
     def hashget(self, name, keys):
+        """
+        哈希查询
+        :param name:
+        :param keys:
+        :return:
+        """
         result = self._redis_pool.hmget(name=name, keys=keys)
         return result
 
     @check_state
     def hashkeys(self, name) -> list:
+        """
+        获取所有哈希keys
+        :param name:
+        :return:
+        """
         result: list = self._redis_pool.hkeys(name)
         return result
 
     @check_state
     def hlen(self, name) -> int:
+        """
+        哈希长度
+        :param name:
+        :return:
+        """
         length = self._redis_pool.hlen(name)
         return length
 
     @check_state
     def lpush(self, name, values: list) -> bool:
+        """
+        list左插入
+        :param name:
+        :param values:
+        :return:
+        """
         pipe = self._redis_pool.pipeline()
         result: bool = self._redis_pool.lpush(name, *values)
         pipe.execute()
@@ -152,6 +185,12 @@ class RedisConnectPool(object):
 
     @check_state
     def rpush(self, name, values: list) -> bool:
+        """
+        list右插入
+        :param name:
+        :param values:
+        :return:
+        """
         pipe = self._redis_pool.pipeline()
         result: bool = self._redis_pool.rpush(name, *values)
         pipe.execute()
@@ -159,6 +198,13 @@ class RedisConnectPool(object):
 
     @check_state
     def lrange(self, name, start, end) -> list:
+        """
+        list左查询
+        :param name:
+        :param start:
+        :param end:
+        :return:
+        """
         max_len = self.llen(name)
         if end > max_len:
             raise AttributeError("索引值过大")
@@ -167,11 +213,22 @@ class RedisConnectPool(object):
 
     @check_state
     def llen(self, name):
+        """
+        list长度
+        :param name:
+        :return:
+        """
         result: int = self._redis_pool.llen(name)
         return result
 
     @check_state
     def zset(self, name, mapping: dict):
+        """
+        set添加
+        :param name:
+        :param mapping: 键值对
+        :return:
+        """
         pipe = self._redis_pool.pipeline()
 
         self._redis_pool.zadd(name, mapping)
@@ -179,6 +236,11 @@ class RedisConnectPool(object):
 
     @check_state
     def zlen(self, name) -> int:
+        """
+        set长度
+        :param name:
+        :return:
+        """
         length: int = self._redis_pool.zcard(name)
         return length
 
@@ -193,6 +255,10 @@ class RedisConnectPool(object):
         return result
 
     def shutdown(self):
+        """
+        关闭连接池
+        :return:
+        """
         self._shutdown = True
 
     def __exit__(self, exc_type, exc_val, exc_tb):
