1e1926fbda62623b78ee489d1fcd9e52fb49abbc
LianZS <2214057556@qq.com>
2019-09-12 16:39:47 +0800

    哈希值追加功能

--- spyderpro/managerfunction/redis_connect.py
+++ spyderpro/managerfunction/redis_connect.py
@@ -1,4 +1,5 @@
 import redis
+import json
 
 
 def check_state(func):
@@ -45,7 +46,7 @@ class RedisConnectPool(object):
         :return:
         """
 
-        pool = redis.ConnectionPool(max_connections=self._max_workers,host='localhost', port=6379)
+        pool = redis.ConnectionPool(max_connections=self._max_workers, host='localhost', port=6379)
         self._redis_pool = redis.Redis(connection_pool=pool)
 
     @check_state
@@ -141,126 +142,156 @@ class RedisConnectPool(object):
         return result
 
     @check_state
-    def hashget(self, name, keys):
+    def hash_value_append(self, name, mapping: dict) -> bool:
         """
-        哈希查询
-        :param name:
-        :param keys:
-        :return:
-        """
-        result = self._redis_pool.hmget(name=name, keys=keys)
-        return result
-
-    @check_state
-    def hashkeys(self, name) -> list:
-        """
-        获取所有哈希keys
-        :param name:
-        :return:
-        """
-        result: list = self._redis_pool.hkeys(name)
-        return result
-
-    @check_state
-    def hlen(self, name) -> int:
-        """
-        哈希长度
-        :param name:
-        :return:
-        """
-        length = self._redis_pool.hlen(name)
-        return length
-
-    @check_state
-    def lpush(self, name, values: list) -> bool:
-        """
-        list左插入
-        :param name:
-        :param values:
+        哈希增加
+        :param name: key
+        :param mapping: 追加的dict
         :return:
         """
         pipe = self._redis_pool.pipeline()
-        result: bool = self._redis_pool.lpush(name, *values)
+        pre_value = self._redis_pool.hgetall(name=name)  # 原先的数据
+        for key, value in pre_value.items():
+            key = key.decode("utf-8")
+            value = value.decode("utf-8")
+            mapping[key] = value
+        result = self._redis_pool.hmset(name, mapping)
         pipe.execute()
         return result
 
-    @check_state
-    def rpush(self, name, values: list) -> bool:
-        """
-        list右插入
-        :param name:
-        :param values:
-        :return:
-        """
-        pipe = self._redis_pool.pipeline()
-        result: bool = self._redis_pool.rpush(name, *values)
-        pipe.execute()
-        return result
 
-    @check_state
-    def lrange(self, name, start, end) -> list:
-        """
-        list左查询
-        :param name:
-        :param start:
-        :param end:
-        :return:
-        """
-        max_len = self.llen(name)
-        if end > max_len:
-            raise AttributeError("索引值过大")
-        result: list = self._redis_pool.lrange(name, start, end)
-        return result
+@check_state
+def hashget(self, name, keys):
+    """
+    哈希查询
+    :param name:
+    :param keys:
+    :return:
+    """
+    result = self._redis_pool.hmget(name=name, keys=keys)
+    return result
 
-    @check_state
-    def llen(self, name):
-        """
-        list长度
-        :param name:
-        :return:
-        """
-        result: int = self._redis_pool.llen(name)
-        return result
 
-    @check_state
-    def zset(self, name, mapping: dict):
-        """
-        set添加
-        :param name:
-        :param mapping: 键值对
-        :return:
-        """
-        pipe = self._redis_pool.pipeline()
+@check_state
+def hashkeys(self, name) -> list:
+    """
+    获取所有哈希keys
+    :param name:
+    :return:
+    """
+    result: list = self._redis_pool.hkeys(name)
+    return result
 
-        self._redis_pool.zadd(name, mapping)
-        pipe.execute()
 
-    @check_state
-    def zlen(self, name) -> int:
-        """
-        set长度
-        :param name:
-        :return:
-        """
-        length: int = self._redis_pool.zcard(name)
-        return length
+@check_state
+def hlen(self, name) -> int:
+    """
+    哈希长度
+    :param name:
+    :return:
+    """
+    length = self._redis_pool.hlen(name)
+    return length
 
-    def zrange_members(self, name) -> list:
-        """
-        获取zset里面的所有元素
-        :param name:
-        :return:
-        """
-        length = self._redis_pool.zcard(name)
-        result: list = self._redis_pool.zrange(name, 0, length, withscores=True)
-        return result
 
-    def shutdown(self):
-        """
-        关闭连接池
-        :return:
-        """
-        self._shutdown = True
+@check_state
+def lpush(self, name, values: list) -> bool:
+    """
+    list左插入
+    :param name:
+    :param values:
+    :return:
+    """
+    pipe = self._redis_pool.pipeline()
+    result: bool = self._redis_pool.lpush(name, *values)
+    pipe.execute()
+    return result
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self._redis_pool.connection_pool.disconnect()
+
+@check_state
+def rpush(self, name, values: list) -> bool:
+    """
+    list右插入
+    :param name:
+    :param values:
+    :return:
+    """
+    pipe = self._redis_pool.pipeline()
+    result: bool = self._redis_pool.rpush(name, *values)
+    pipe.execute()
+    return result
+
+
+@check_state
+def lrange(self, name, start, end) -> list:
+    """
+    list左查询
+    :param name:
+    :param start:
+    :param end:
+    :return:
+    """
+    max_len = self.llen(name)
+    if end > max_len:
+        raise AttributeError("索引值过大")
+    result: list = self._redis_pool.lrange(name, start, end)
+    return result
+
+
+@check_state
+def llen(self, name):
+    """
+    list长度
+    :param name:
+    :return:
+    """
+    result: int = self._redis_pool.llen(name)
+    return result
+
+
+@check_state
+def zset(self, name, mapping: dict):
+    """
+    set添加
+    :param name:
+    :param mapping: 键值对
+    :return:
+    """
+    pipe = self._redis_pool.pipeline()
+
+    self._redis_pool.zadd(name, mapping)
+    pipe.execute()
+
+
+@check_state
+def zlen(self, name) -> int:
+    """
+    set长度
+    :param name:
+    :return:
+    """
+    length: int = self._redis_pool.zcard(name)
+    return length
+
+
+def zrange_members(self, name) -> list:
+    """
+    获取zset里面的所有元素
+    :param name:
+    :return:
+    """
+    length = self._redis_pool.zcard(name)
+    result: list = self._redis_pool.zrange(name, 0, length, withscores=True)
+    return result
+
+
+def shutdown(self):
+    """
+    关闭连接池
+    :return:
+    """
+    self._shutdown = True
+
+
+def __exit__(self, exc_type, exc_val, exc_tb):
+    self._redis_pool.connection_pool.disconnect()
