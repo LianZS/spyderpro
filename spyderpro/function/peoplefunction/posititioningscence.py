8be2427e5fefdad1977a38a6bdc393fbccb318a2
LianZS <2214057556@qq.com>
2019-09-12 10:35:14 +0800

    缓存以及数据录入bug修改

--- spyderpro/function/peoplefunction/posititioningscence.py
+++ spyderpro/function/peoplefunction/posititioningscence.py
@@ -36,7 +36,7 @@ class Parent(MysqlOperation):
 
 class ScenceFlow(Parent):
 
-    def get_scence_situation(self, db, peoplepid) -> List[Positioning]:
+    def get_scence_situation(self, db, peoplepid, table_id) -> List[Positioning]:
         """
         请求某个景区实时客流量
         :param peoplepid: 景区id
@@ -47,50 +47,54 @@ class ScenceFlow(Parent):
         # 获取数据
         instances = flow.peopleflow_info(peoplepid, ddate)
         # 缓冲数据
-        self.__redis_cache(instances)
+        positioning_data = list(instances)
+        self.__redis_cache(positioning_data)
         # 过滤已存在的数据
-        info = self.__filter_peopleflow(db, instances, ddate, peoplepid)
+        info = self.__filter_peopleflow(db, positioning_data, ddate, peoplepid, table_id)
 
         return info
 
-    def __redis_cache(self, data_objs):
+    @staticmethod
+    def __redis_cache(data_objs):
         """
         缓存数据,缓存key格式为 scence:pid
         :param data_objs: 数据实例
         :return:
         """
-        dict_data = dict()
+        dict_data = dict()  # {"HH:MM:SS":num}
+        pid = 0
         for obj in data_objs:
             pid = obj.region_id
             detail_time = obj.detailTime
             num = obj.num
             dict_data[detail_time] = num
         redis_pool = RedisConnectPool(max_workers=1)
-        key = "scence:" + pid
+        key = "scence:{0}".format(pid)
         redis_pool.hashset(name=key, mapping=dict_data)
 
     # 检查数据库是否存在部分数据，存在则不再插入
-    def __filter_peopleflow(self, db_connect, objs, ddate: int, peoplepid: int) -> List[Positioning]:
+    def __filter_peopleflow(self, db_connect, objs: List[Positioning], ddate: int, peoplepid: int, table_id: int) -> \
+            List[Positioning]:
         """
         检查数据库是否存在部分数据，存在则不再插入
         :param db_connect:  数据库实例
-        :param objs: Positioning迭代器
+        :param objs: Positioning列表
         :param ddate: 日期
         :param peoplepid: 数据库查询条件
         :return: list(Positioning)
         """
-
-        sql = "select ttime from digitalsmart.scenceflow where pid={0} and  ddate={1} ".format(peoplepid,
-                                                                                               ddate)
-        cursor = self.get_cursor(db_connect, sql)  # 从数据库获取当天已存在的数据
+        sql_cmd = "select ttime from digitalsmart.historyscenceflow{table_id} where pid={pid} and  ddate={ddate} ".format(
+            table_id=table_id, pid=peoplepid,
+            ddate=ddate)
+        cursor = self.get_cursor(db_connect, sql_cmd)  # 从数据库获取当天已存在的数据
         if cursor == "error":
             return []
         data = cursor.fetchall()  # 从数据库获取已经存在的数据
 
         cursor.close()
         dic = {}
-        for info in objs:
-            dic[info.detailTime] = info.num
+        for info in objs:  # 构造对象字典
+            dic[info.detailTime] = info
 
         for item in data:  # 将存在的数据淘汰掉
             item = str(item[0])
@@ -200,7 +204,7 @@ if __name__ == "__main__":
     p = ScenceFlow()
     pool = ConnectPool(max_workers=1)
     db = pool.work_queue.get()
-    p.get_scence_situation(db, 1365)
+    p.get_scence_situation(db, 1365,555)
     # db = pymysql.connect(host=host, user=user, password=password, database=scencedatabase,
     #                      port=port)
     # data = p.get_scence_situation(db, 1174)
