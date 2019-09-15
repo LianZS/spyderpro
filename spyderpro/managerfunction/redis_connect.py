47a29efa5011d2dcf432eae8a41ef7b57e43c77b
LianZS <2214057556@qq.com>
2019-09-16 07:04:49 +0800

    解决redis连接池出现During handling of the above exception, another exception occurred: too many connect

--- spyderpro/managerfunction/redis_connect.py
+++ spyderpro/managerfunction/redis_connect.py
@@ -1,5 +1,5 @@
 import redis
-import json
+from threading import Semaphore
 
 
 def check_state(func):
@@ -12,7 +12,11 @@ def check_state(func):
     def wrapper(self, *args, **kwargs):
         if self._shutdown:
             raise RuntimeError('连接池已经关闭，无法创建新的连接')
-        return func(self, *args, **kwargs)
+        self._semaphore.acquire()
+        result = func(self, *args, **kwargs)
+        self._semaphore.release()
+
+        return result
 
     return wrapper
 
@@ -39,6 +43,7 @@ class RedisConnectPool(object):
             self._broken = False
             self._shutdown = False
             self._init_pool_()
+            self._semaphore = Semaphore(10)#解决并发时too many connect 错误
 
     def _init_pool_(self):
         """
@@ -79,11 +84,18 @@ class RedisConnectPool(object):
             if it already exists.
         :return:
         """
-        pipe = self._redis_pool.pipeline()
-        resuslt: bool = self._redis_pool.set(name, value, ex=ex, px=px, nx=nx, xx=xx)
-        pipe.execute()
 
-        return resuslt
+        with self._redis_pool.pipeline() as pipe:
+            try:
+
+                pipe.watch(name)
+                pipe.multi()
+                resuslt: bool = pipe.set(name, value, ex=ex, px=px, nx=nx, xx=xx)
+                pipe.execute()
+                return resuslt
+
+            except redis.exceptions.WatchError:
+                print("%s --WatchError" % name)
 
     @check_state
     def ttl(self, name) -> int:
@@ -136,10 +148,16 @@ class RedisConnectPool(object):
         :param mapping: 键值对
         :return:
         """
-        pipe = self._redis_pool.pipeline()
-        result = self._redis_pool.hmset(name, mapping)
-        pipe.execute()
-        return result
+        with self._redis_pool.pipeline() as pipe:
+            try:
+                pipe.watch(name)
+                pipe.multi()
+                resuslt: bool = pipe.hset(name, mapping)
+                pipe.execute()
+                return resuslt
+
+            except redis.exceptions.WatchError:
+                print("%s --WatchError" % name)
 
     @check_state
     def hash_value_append(self, name, mapping: dict) -> bool:
@@ -149,149 +167,161 @@ class RedisConnectPool(object):
         :param mapping: 追加的dict
         :return:
         """
-        pipe = self._redis_pool.pipeline()
-        pre_value = self._redis_pool.hgetall(name=name)  # 原先的数据
-        for key, value in pre_value.items():
-            key = key.decode("utf-8")
-            value = value.decode("utf-8")
-            mapping[key] = value
-        result = self._redis_pool.hmset(name, mapping)
-        pipe.execute()
+        with self._redis_pool.pipeline() as pipe:
+            pipe.watch(name)
+            pre_value = pipe.hgetall(name=name)  # 原先的数据
+            for key, value in pre_value.items():
+                key = key.decode("utf-8")
+                value = value.decode("utf-8")
+                mapping[key] = value
+            result = self._redis_pool.hmset(name, mapping)
+            pipe.execute()
+            return result
+
+    @check_state
+    def hashget(self, name, keys):
+        """
+        哈希查询
+        :param name:
+        :param keys:
+        :return:
+        """
+        result = self._redis_pool.hmget(name=name, keys=keys)
         return result
 
+    @check_state
+    def hashkeys(self, name) -> list:
+        """
+        获取所有哈希keys
+        :param name:
+        :return:
+        """
+        result: list = self._redis_pool.hkeys(name)
+        return result
 
-@check_state
-def hashget(self, name, keys):
-    """
-    哈希查询
-    :param name:
-    :param keys:
-    :return:
-    """
-    result = self._redis_pool.hmget(name=name, keys=keys)
-    return result
+    @check_state
+    def hlen(self, name) -> int:
+        """
+        哈希长度
+        :param name:
+        :return:
+        """
+        length = self._redis_pool.hlen(name)
+        return length
 
+    @check_state
+    def lpush(self, name, values: list) -> bool:
+        """
+        list左插入
+        :param name:
+        :param values:
+        :return:
+        """
+        with self._redis_pool.pipeline() as pipe:
+            try:
 
-@check_state
-def hashkeys(self, name) -> list:
-    """
-    获取所有哈希keys
-    :param name:
-    :return:
-    """
-    result: list = self._redis_pool.hkeys(name)
-    return result
+                pipe.watch(name)
+                pipe.multi()
+                resuslt: bool = pipe.lpush(name, *values)
+                pipe.execute()
+                return resuslt
 
-
-@check_state
-def hlen(self, name) -> int:
-    """
-    哈希长度
-    :param name:
-    :return:
-    """
-    length = self._redis_pool.hlen(name)
-    return length
-
-
-@check_state
-def lpush(self, name, values: list) -> bool:
-    """
-    list左插入
-    :param name:
-    :param values:
-    :return:
-    """
-    pipe = self._redis_pool.pipeline()
-    result: bool = self._redis_pool.lpush(name, *values)
-    pipe.execute()
-    return result
+            except redis.exceptions.WatchError:
+                print("%s --WatchError" % name)
 
 
-@check_state
-def rpush(self, name, values: list) -> bool:
-    """
-    list右插入
-    :param name:
-    :param values:
-    :return:
-    """
-    pipe = self._redis_pool.pipeline()
-    result: bool = self._redis_pool.rpush(name, *values)
-    pipe.execute()
-    return result
+    @check_state
+    def rpush(self, name, values: list) -> bool:
+        """
+        list右插入
+        :param name:
+        :param values:
+        :return:
+        """
+        with self._redis_pool.pipeline() as pipe:
+            try:
+
+                pipe.watch(name)
+                pipe.multi()
+                resuslt: bool = pipe.rpush(name, *values)
+                pipe.execute()
+                return resuslt
+
+            except redis.exceptions.WatchError:
+                print("%s --WatchError" % name)
 
 
-@check_state
-def lrange(self, name, start, end) -> list:
-    """
-    list左查询
-    :param name:
-    :param start:
-    :param end:
-    :return:
-    """
-    max_len = self.llen(name)
-    if end > max_len:
-        raise AttributeError("索引值过大")
-    result: list = self._redis_pool.lrange(name, start, end)
-    return result
+    @check_state
+    def lrange(self, name, start, end) -> list:
+        """
+        list左查询
+        :param name:
+        :param start:
+        :param end:
+        :return:
+        """
+        max_len = self.llen(name)
+        if end > max_len:
+            raise AttributeError("索引值过大")
+        result: list = self._redis_pool.lrange(name, start, end)
+        return result
 
+    @check_state
+    def llen(self, name):
+        """
+        list长度
+        :param name:
+        :return:
+        """
+        result: int = self._redis_pool.llen(name)
+        return result
 
-@check_state
-def llen(self, name):
-    """
-    list长度
-    :param name:
-    :return:
-    """
-    result: int = self._redis_pool.llen(name)
-    return result
+    @check_state
+    def zset(self, name, mapping: dict):
+        """
+        set添加
+        :param name:
+        :param mapping: 键值对
+        :return:
+        """
+        with self._redis_pool.pipeline() as pipe:
+            try:
 
+                pipe.watch(name)
+                pipe.multi()
+                resuslt: bool = pipe.zadd(name, mapping)
+                pipe.execute()
+                return resuslt
 
-@check_state
-def zset(self, name, mapping: dict):
-    """
-    set添加
-    :param name:
-    :param mapping: 键值对
-    :return:
-    """
-    pipe = self._redis_pool.pipeline()
+            except redis.exceptions.WatchError:
+                print("%s --WatchError" % name)
 
-    self._redis_pool.zadd(name, mapping)
-    pipe.execute()
+    @check_state
+    def zlen(self, name) -> int:
+        """
+        set长度
+        :param name:
+        :return:
+        """
+        length: int = self._redis_pool.zcard(name)
+        return length
 
+    def zrange_members(self, name) -> list:
+        """
+        获取zset里面的所有元素
+        :param name:
+        :return:
+        """
+        length = self._redis_pool.zcard(name)
+        result: list = self._redis_pool.zrange(name, 0, length, withscores=True)
+        return result
 
-@check_state
-def zlen(self, name) -> int:
-    """
-    set长度
-    :param name:
-    :return:
-    """
-    length: int = self._redis_pool.zcard(name)
-    return length
+    def shutdown(self):
+        """
+        关闭连接池
+        :return:
+        """
+        self._shutdown = True
 
-
-def zrange_members(self, name) -> list:
-    """
-    获取zset里面的所有元素
-    :param name:
-    :return:
-    """
-    length = self._redis_pool.zcard(name)
-    result: list = self._redis_pool.zrange(name, 0, length, withscores=True)
-    return result
-
-
-def shutdown(self):
-    """
-    关闭连接池
-    :return:
-    """
-    self._shutdown = True
-
-
-def __exit__(self, exc_type, exc_val, exc_tb):
-    self._redis_pool.connection_pool.disconnect()
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._redis_pool.connection_pool.disconnect()
