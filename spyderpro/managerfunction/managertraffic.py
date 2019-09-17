bb9ceab5b387beb4bc8ef654883dfbbde86bd4d8
LianZS <2214057556@qq.com>
2019-09-17 23:13:20 +0800

    道路交通采集性能提升

--- spyderpro/managerfunction/managertraffic.py
+++ spyderpro/managerfunction/managertraffic.py
@@ -81,6 +81,7 @@ class ManagerTraffic(Traffic):
         sql = "select pid from digitalsmart.citymanager"
 
         data = pool.select(sql)  # pid集合
+        thread_pool = ThreadPool(max_workers=3)
         for item in data:  # 这里最好不要并发进行，因为每个pid任务下都有10个子线程，在这里开并发 的话容易被封杀
 
             pid = item[0]
@@ -117,9 +118,11 @@ class ManagerTraffic(Traffic):
                         "road_id": roadid, "rate": rate
                     }
                     self._redis_worker.set(redis_key, str(mapping))
-                    print(mapping)
 
-            fast(pid)
+            if pid < 1000:
+                thread_pool.submit(fast, pid)
+            else:
+                fast(pid)
 
         print("城市道路交通数据挖掘完毕")
 
@@ -173,4 +176,4 @@ if __name__ == "__main__":
 
     m = ManagerTraffic()
     Process(target=m.manager_city_road_traffic).start()
-    Process(target=m.manager_city_traffic).start()
+    # Process(target=m.manager_city_traffic).start()
