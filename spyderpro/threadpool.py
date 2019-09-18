fe43566ee1bd0939bddc060b9d610432f36d221e
LianZS <2214057556@qq.com>
2019-09-18 13:28:45 +0800

    自定义线程池

--- spyderpro/threadpool.py
+++ spyderpro/threadpool.py
@@ -1,6 +1,5 @@
 import os
-import threading
-from threading import Thread, Lock, Semaphore
+from threading import Thread, Event, Lock
 
 from queue import Queue
 
@@ -29,12 +28,15 @@ class ThreadPool():
             max_workers = (os.cpu_count() or 1) * 5
         if max_workers <= 0:
             raise ValueError("最大线程数必须大于0")
-        self._max_workers = max_workers
-        self._work_queue = Queue(max_workers)  # 存放任务
-        self._broken = False
-        self._shutdown = False
-        self._shutdown_lock = Lock()
-        self._create_thread()
+        self._max_workers = max_workers  # 最大线程数
+        self._work_queue = Queue(max_workers)  # 任务队列
+        self._work_list = list()  # 任务列表
+        self._stop = Queue(max_workers)  # 线程是否关闭消息队列
+        self._shutdown = False  # 是否关闭线程池
+        self._thread_list = list()  # 线程列表
+
+        self._create_thread()  # 初始化线程池
+        self._state = False  # 是否执行过一遍任务，避免反复执行
 
     def _create_thread(self):
         """
@@ -42,7 +44,9 @@ class ThreadPool():
         :return:
         """
         for i in range(self._max_workers):
-            Thread(target=self._adjust_thread_count, args=()).start()
+            t = Thread(target=self._adjust_thread_count, args=())
+            self._thread_list.append(t)
+            t.start()
 
     def submit(self, fn, *args, **kwargs):
         """
@@ -54,17 +58,31 @@ class ThreadPool():
         """
         if self._shutdown:
             raise RuntimeError('线程池已经关闭，无法创建新的线程')
+        self._state = False
         w = _Worker(fn, args, kwargs)
-        self._work_queue.put(w)  # 任务队列
+        self._work_list.append(w)
+
+    def run(self):
+        """
+        启动任务
+        :return:
+        """
+        if self._state:
+            raise BaseException("任务已经过一遍，不可重复执行")
+        for w in self._work_list:
+            self._stop.put(1)
+            self._work_queue.put(w)
+        self._state = True  # 设置该任务集已经执行过一遍了
+        self._work_list.clear()
+        # self.close()
 
     def _adjust_thread_count(self):
         """
         监听线程
-        :return:
         """
-        while 1:
-            w = self._work_queue.get()
-            w.run()
+        while self._stop.get():
+            w = self._work_queue.get()  # 获取任务
+            w.run()  # 执行任务
 
     def shut_down(self):
         """
@@ -72,6 +90,14 @@ class ThreadPool():
         :return:
         """
         self._shutdown = True
+
+    def close(self):
+        """
+        终止每个线程
+        :return:
+        """
+        for i in range(self._max_workers):
+            self._stop.put(0)  # 通知关闭线程
 
 
 def test(i):
@@ -85,4 +111,7 @@ if __name__ == "__main__":
     pool = ThreadPool(max_workers=5)
     for k in range(14):
         pool.submit(test, k)
-    pool.shut_down()
+    pool.run()
+    pool.submit(test,100)
+    pool.run()
+    pool.close()
