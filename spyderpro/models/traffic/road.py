198db071384538a08bf4075a6e912d4a406d93f5
LianZS <2214057556@qq.com>
2019-09-23 22:43:16 +0800

    道路数据RoadData类新增属性

--- spyderpro/models/traffic/road.py
+++ spyderpro/models/traffic/road.py
@@ -24,10 +24,11 @@ class RoadData:
     一条道路拥有的具体数据--拥堵排名，时间，交通数据
     """
 
-    def __init__(self, num: int, detailt_time_list: list, road_data_list: list):
+    def __init__(self, num: int, detailt_time_list, road_data_list, coords=None):
         self.num: int = num
-        self.detailt_time_list: list = detailt_time_list
-        self.road_data_list: list = road_data_list
+        self.detailt_time_list = detailt_time_list
+        self.road_data_list = road_data_list
+        self.coords = coords  # 道路经纬度
 
     def __str__(self):
         return "排名：{num}，时间：{ttime}，道路数据集：{data}".format(num=self.num, ttime=self.detailt_time_list,
@@ -48,10 +49,9 @@ class Road:
         data : Dict类型，用于记录拥堵数据集，格式为{"num": 0, "time": ["02:50", "02:55", ], "data": ["1.05", "1.06", ]}
 
     """
-    __slots__ = ['region_id', 'roadname', 'speed', 'direction', 'bounds', 'data', "num", "rate"]
 
-    def __init__(self, pid: int, roadname: str, speed: float, dircetion: str, bounds: dict, data: dict, num: int,
-                 rate: float):
+    def __init__(self, pid: int, roadname: str, speed: float, dircetion: str, bounds: dict, road_index_data_list: list,
+                 time_data_list: list, num: int, rate: float):
         """
         Args:
             pid: INT 类型属性，城市标识
@@ -59,7 +59,8 @@ class Road:
             speed:FLOAT类型,速度
             dircetion:String类型，方向
             bounds:Dict类型，经纬度范围，格式为{"coords": [{"lon": "120.324071", "lat": "31.633172"}，，，，]}
-            data: Dict类型，拥堵数据集，格式为{"num": 0, "time": ["02:50", "02:55", ], "data": ["1.05", "1.06", ]}
+            road_index_data_list: List，拥堵数据集，格式["1.05", "1.06", ]
+            time_data_list:List，格式【"00"】
             num:INT类型，表示道路排在第几名
             rate:Float类型表示拥堵指数
 
@@ -69,11 +70,16 @@ class Road:
         self.speed: float = speed
         self.direction: str = dircetion
         self.bounds: dict = bounds
-        self.data: dict = data
+        self.road_index_data_list: list = road_index_data_list
+        self.time_data_list: list = time_data_list
         self.num: int = num
         self.rate: float = rate
 
     def __str__(self):
-        return "标识:{0},路名：{1}，拥堵指数:{2},方向：{3}，速度：{4},道路经纬度：{5}".format(self.region_id, self.roadname, self.rate,
-                                                                       self.direction,
-                                                                       self.speed, self.bounds)
+        return "标识:{pid},路名：{road_name}，拥堵指数:{cur_rate},方向：{dir}，速度：{speed},时间集合:{times}，拥堵数据集：" \
+               "{indexs},道路经纬度：{coords}".format(pid=self.region_id, road_name=self.roadname,
+                                                cur_rate=self.rate,
+                                                dir=self.direction,
+                                                speed=self.speed, indexs=self.road_index_data_list,
+                                                times=self.time_data_list,
+                                                coords=self.bounds)
