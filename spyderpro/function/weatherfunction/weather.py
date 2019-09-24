7730b29d7a3882f95bf8cf7786ae88f5c5a05824
LianZS <2214057556@qq.com>
2019-09-24 20:55:58 +0800

    返回类型检查

--- spyderpro/function/weatherfunction/weather.py
+++ spyderpro/function/weatherfunction/weather.py
@@ -1,8 +1,12 @@
-from typing import Dict
+from typing import Iterator
 from spyderpro.models.weather.airstate import AirState
-class Weather:
-    def get_city_weather_pid(self)->Dict:
-        return AirState().get_city_air_pid()
-    def last_air_state(self,citypid):
+from spyderpro.models.weather.aqi import AQIState, InfoOfCityOfAQI
 
+
+
+class Weather:
+    def get_city_weather_pid(self) -> Iterator[InfoOfCityOfAQI]:
+        return AirState().get_city_air_pid()
+
+    def last_air_state(self, citypid)->AQIState:
         return AirState().get_city_air_state(citypid)
