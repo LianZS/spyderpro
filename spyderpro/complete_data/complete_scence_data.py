064a1f863d81a0cce53fe0656607875614e89a3d
LianZS <2214057556@qq.com>
2019-09-25 20:34:41 +0800

    人流趋势数据缺失补全功能

--- spyderpro/complete_data/complete_scence_data.py
+++ spyderpro/complete_data/complete_scence_data.py
@@ -1,11 +1,15 @@
 import datetime
 from spyderpro.pool.redis_connect import RedisConnectPool
 from spyderpro.data_requests.scence.place_people_num import PlacePeopleNum
+from spyderpro.data_requests.scence.place_people_trend import PlaceTrend
 
 
 class CompleteScenceData:
     def __init__(self):
-        self._redis_work = RedisConnectPool(max_workers=2)
+        self._redis_work = RedisConnectPool(max_workers=1)
+
+    def __del__(self):
+        del self._redis_work
 
     def complete_scence_people_num_data(self, key: str, pid: int, missing_time: dict):
         """
@@ -22,7 +26,30 @@ class CompleteScenceData:
         for miss_time in missing_time.keys():
             # 请求热力图数据
             response_data = place.get_heatdata_bytime(ddate=today_ddate, date_time=miss_time, region_id=pid)
+            if not response_data:
+                continue
             # 统计数据
             positioning = place.count_headdata(response_data, today_ddate, miss_time, pid)
             # 缓存追加
             self._redis_work.hash_value_append(key, {positioning.detailTime: positioning.num})
+
+    def complete_scence_people_trend_data(self, key: str, area_name: str, pid: int):
+        """
+
+        :param key: 缓存key
+        :param area_name: 景区名
+        :param pid: 景区标识
+        :return:
+        """
+        now = datetime.datetime.today()
+        time_interval = datetime.timedelta(days=1)
+        tomorrow = now + time_interval
+        today = str(now.date())
+        tomorrow_day = str(tomorrow.date())
+
+        trend = PlaceTrend(today, tomorrow_day)
+        result_objs = trend.get_trend(area_name, pid)
+        redis_data = dict()
+        for obj in result_objs:
+            redis_data[obj.detailtime] = obj.index
+        self._redis_work.hashset(key, redis_data)
