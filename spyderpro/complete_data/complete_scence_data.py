d36f76e845f2e17de506581435f4b154bfee78c5
LianZS <2214057556@qq.com>
2019-09-25 13:56:42 +0800

    补全内存缺失的数据

---
+++ spyderpro/complete_data/complete_scence_data.py
@@ -0,0 +1,12 @@
+class CompleteScenceData:
+    def __init__(self):
+        pass
+
+    def complete_scence_people_num_data(self, key, missing_time):
+        """
+        补全缺失的数据
+        :param key: 缓存key
+        :param missing_time:缺失数据的时间点
+        :return:
+        """
+        pass
