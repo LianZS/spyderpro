a9692694b2ea9583269023a12eb0d45b187b1e45
LianZS <2214057556@qq.com>
2019-09-26 00:30:02 +0800

    并发处理数据缺失

--- spyderpro/complete_data/complete_scence_data.py
+++ spyderpro/complete_data/complete_scence_data.py
@@ -1,12 +1,15 @@
 import datetime
+from queue import Queue
 from spyderpro.pool.redis_connect import RedisConnectPool
 from spyderpro.data_requests.scence.place_people_num import PlacePeopleNum
 from spyderpro.data_requests.scence.place_people_trend import PlaceTrend
+from spyderpro.pool.threadpool import ThreadPool
 
 
 class CompleteScenceData:
     def __init__(self):
         self._redis_work = RedisConnectPool(max_workers=1)
+        self._queue = Queue()
 
     def __del__(self):
         del self._redis_work
@@ -19,28 +22,49 @@ class CompleteScenceData:
         :param pid:景区标识
         :return:
         """
+        time_interval = datetime.timedelta(minutes=60)
+        pool = ThreadPool(max_workers=10)
+        for miss_time in missing_time.keys():
+            pool.submit(self._fast, pid, miss_time)
+        pool.run()
+        pool.close()
+        mapping = dict()  # 存放需要缓存的数据
+        # while 1:
+        #     try:
+        #
+        #         positioning = self._queue.get(timeout=2)  # 所有数据之前已经放在队列里了
+        #         mapping[positioning.detailTime] = positioning.num
+        #     except Exception as e:
+        #         print(e)
+        #         break
+        "存在数据返回问题"
+        #  缓存追加
+        pool.result  # 并发返回结果
+        # self._redis_work.hash_value_append(key, mapping)
+        # self._redis_work.expire(key, time_interval)
 
+    def _fast(self, pid: int, miss_time: str):
         today = datetime.datetime.today()
         today_ddate = str(today.date())
         place = PlacePeopleNum()
-        for miss_time in missing_time.keys():
-            # 请求热力图数据
-            response_data = place.get_heatdata_bytime(ddate=today_ddate, date_time=miss_time, region_id=pid)
-            if not response_data:
-                continue
-            # 统计数据
-            positioning = place.count_headdata(response_data, today_ddate, miss_time, pid)
-            # 缓存追加
-            self._redis_work.hash_value_append(key, {positioning.detailTime: positioning.num})
+        response_data = place.get_heatdata_bytime(ddate=today_ddate, date_time=miss_time, region_id=pid)
+        if not response_data:
+            return
+        # 统计数据
+        positioning = place.count_headdata(response_data, today_ddate, miss_time, pid)
+        return positioning
+        # self._queue.put(positioning)
 
     def complete_scence_people_trend_data(self, key: str, area_name: str, pid: int):
         """
-
+         补全数据，这里直接将之前所有的数据写入
         :param key: 缓存key
         :param area_name: 景区名
         :param pid: 景区标识
         :return:
         """
+        # 缓存时间
+        redis_time = datetime.timedelta(minutes=60)
         now = datetime.datetime.today()
         time_interval = datetime.timedelta(days=1)
         tomorrow = now + time_interval
@@ -48,8 +72,11 @@ class CompleteScenceData:
         tomorrow_day = str(tomorrow.date())
 
         trend = PlaceTrend(today, tomorrow_day)
+        # 获取这天的趋势数据
         result_objs = trend.get_trend(area_name, pid)
         redis_data = dict()
         for obj in result_objs:
             redis_data[obj.detailtime] = obj.index
+        # 缓存
         self._redis_work.hashset(key, redis_data)
+        self._redis_work.expire(key, redis_time)
